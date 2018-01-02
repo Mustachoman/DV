dba420fee24d94dc793394e45ad572d25e23063c
Marijn van Straten <m.vanstraten@link-it.nl>
2018-01-02 17:23:53 +0100

    Removed the countries that have no starbucks data.

--- Source/JS/happinessdata.js
+++ Source/JS/happinessdata.js
@@ -7,7 +7,8 @@ d3.csv("Data/2017.csv", function (data) {
 let happinessloadInterval = setInterval(setHappinessCountryCodes, 1000);
 
 function setHappinessCountryCodes() {
-    if (happinessData !== undefined && countryCodes !== undefined) {
+    let indexesToRemove = [];
+    if (happinessData !== undefined && countryCodes !== undefined && storeCount !== undefined) {
         for (country in happinessData) {
             let oldCountryCode = happinessData[country]['Country'];
             let newCountryCode = '';
@@ -15,10 +16,17 @@ function setHappinessCountryCodes() {
                 if (countryCodes[countryCode]['official_name_en'] == oldCountryCode) {
                     newCountryCode = countryCodes[countryCode]['ISO3166-1-Alpha-3'];
                     happinessData[country]['Country3'] = newCountryCode;
+                    if (Object.keys(storeCount).indexOf(newCountryCode) == -1) {
+                        indexesToRemove.push(country)
+                    }
                 }
             }
         }
-        clearInterval(happinessloadInterval);
-        extractGDPData();
+
+        while (indexesToRemove.length) {
+            happinessData.splice(indexesToRemove.pop(), 1);
+        }
     }
+    clearInterval(happinessloadInterval);
+    extractGDPData();
 }
