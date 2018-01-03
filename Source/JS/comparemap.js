f4e802b4c2b0e23b2dc6f1e93c1d8068ee953984
Marijn van Straten <m.vanstraten@link-it.nl>
2018-01-03 23:38:09 +0100

    compare map / functionality added

---
+++ Source/JS/comparemap.js
@@ -0,0 +1,45 @@
+let compareData = {};
+
+let comparemap = new Datamap({
+    element: document.getElementById('comparemap'),
+    geographyConfig: {
+        popupTemplate: function (geography, data) {
+            return '<div class="hoverinfo"><b>' + geography.properties.name + '</b></div>'
+        }
+    },
+    fills: {
+        defaultFill: '#efe5de'
+    },
+    projection: 'mercator'
+});
+
+function updatecompareMap() {
+    let starbucksScore = {};
+
+    let storeModifier = d3.select("#nudStores").property("value");
+    let gdpModifier = d3.select("#nudGDP").property("value");
+
+    let storeScoreArray = Object.keys(storeCount).sort((a, b) => { return storeCount[b] - storeCount[a] }).reverse();
+    let gdpScoreArray = Object.keys(countryGDP).sort((a, b) => { return countryGDP[b] - countryGDP[a] }).reverse();
+
+    allStores.forEach((store) => {
+        countryName = store['Country3'];
+        let storeScore = storeScoreArray.indexOf(countryName) * storeModifier;
+        let gdpScore = gdpScoreArray.indexOf(countryName) * gdpModifier;
+        starbucksScore[countryName] = storeScore + gdpScore;
+    });
+
+    var paletteScale = d3.scale.log()
+        .domain([Math.min(...Object.values(starbucksScore)), Math.max(...Object.values(starbucksScore))])
+        .range(["#efffef", "#00713D"]);
+
+    for (country in starbucksScore) {
+        var iso = country,
+            value = starbucksScore[country];
+        compareData[iso] = { amount: value, fillColor: paletteScale(value) };
+    }
+
+    comparemap.updateChoropleth(countMapData);
+
+    console.log(starbucksScore);
+}
